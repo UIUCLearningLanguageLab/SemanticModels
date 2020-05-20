7978da5d2c8426823451a3151ddfa336431df249
jwillits@illinois.edu <jwillits@illinois.edu>
2020-05-20 09:49:52 -0500

    changes to sim and neighbors

--- src/models/distributional_model.py
+++ src/models/distributional_model.py
@@ -8,6 +8,7 @@ import my_utils
 
 class DistributionalModel:
 
+    #############################################################################################################################
     def __init__(self):
         self.corpus = None
         
@@ -24,9 +25,10 @@ class DistributionalModel:
         self.reduction_method = None
         self.reduction_size = None
 
+        self.similarity_metric = None
         self.similarity_matrix = None
     
-    # in the DM base class
+    #############################################################################################################################
     def create_model_name(self, model_type='dm'):
         self.start_datetime = datetime.datetime.timetuple(datetime.datetime.now())
         self.model_name = "{}_{}_{}_{}_{}_{}_{}".format(model_type, self.start_datetime[1],
@@ -37,6 +39,7 @@ class DistributionalModel:
                                                self.start_datetime[6])
         self.model_path = '../models/' + self.model_name
 
+    #############################################################################################################################
     def create_model_directory(self):
         if not os.path.isdir("../models/"):
             os.mkdir("../models/")
@@ -47,6 +50,7 @@ class DistributionalModel:
         else:
             os.mkdir(self.model_path)
 
+    #############################################################################################################################
     def create_stop_list(self):
         if self.stop_list_path is not None:
             # here is where you would create a stop list from a file
@@ -54,6 +58,7 @@ class DistributionalModel:
         else:
             self.stop_list = []
 
+    #############################################################################################################################
     def create_vocabulary(self, num_vocab):
         self.num_vocab = 0
         self.vocab_list = []
@@ -85,6 +90,7 @@ class DistributionalModel:
         self.vocab_index_dict['UNKNOWN'] = self.num_vocab
         self.num_vocab += 1
 
+    #############################################################################################################################s
     def create_model_config_files(self):
         f = open(self.model_path+'/config.txt', 'w')
         f.write("model_name: {}\n".format(self.model_name))
@@ -98,7 +104,7 @@ class DistributionalModel:
             f.write("{},{}\n".format(i, self.vocab_list[i]))
         f.close()
 
-    # in the DM base class
+    #############################################################################################################################
     def normalize_embedding_matrix(self):
         print("    Normalizing using method:", self.normalization_method)
         if self.normalization_method == None:
@@ -115,11 +121,10 @@ class DistributionalModel:
             self.vocab_embedding_matrix = my_utils.row_log_entropy_normalize(self.vocab_embedding_matrix)
         elif self.normalization_method == 'ppmi':
             self.vocab_embedding_matrix = my_utils.ppmi_normalize(self.vocab_embedding_matrix)
-            # dog-cat 1.0 2.0  0
-            # dog-d1 2.0
         else:
             raise AttributeError('Invalid arg {} to normalization_method'.format(self.normalization_method))
 
+    #############################################################################################################################
     def reduce_embedding_matrix(self):
         print("Reducing embedding matrix using method", self.reduction_method)
         if self.reduction_method is None:
@@ -131,22 +136,31 @@ class DistributionalModel:
         else:
             raise AttributeError('Invalid arg {} to reduction_method'.format(self.reduction_method))
 
+    #############################################################################################################################
     def save_model(self):
         pickle_file = open(self.model_path + '/model_object.p', 'wb')
         pickle.dump(self, pickle_file)
         pickle_file.close()
 
+    #############################################################################################################################
     def save_embedding_matrix(self):
         np.savetxt(self.model_path+'/embedding_matrix.csv', self.vocab_embedding_matrix, delimiter=",", fmt="%0.4f")
     
+    #############################################################################################################################
     def load_model(self, model_name):
         pickle_file = open('../models/'+model_name+'/model_object.p', 'rb')
         instance_object = pickle.load(pickle_file)
         pickle_file.close()
         self.__dict__ = instance_object.__dict__
     
+    #############################################################################################################################
     def compute_full_similarity_matrix(self, similarity_metric):
+        sim_file_path = self.model_path + '/similarities_' + similarity_metric + '.csv'
+        if os.path.exists(sim_file_path):
+            raise AttributeError('{} Similarity file for model {} already exists'.format(similarity_metric, self.model_name))
+
         print("Computing all similarities")
+        self.similarity_metric = similarity_metric
         self.similarity_matrix = np.zeros([self.num_vocab, self.num_vocab])
         for i in range(self.num_vocab):
             a = self.vocab_embedding_matrix[i, :]
@@ -161,11 +175,11 @@ class DistributionalModel:
                             self.similarity_matrix[j, i] = self.similarity_matrix[i, j]
                         elif similarity_metric == 'euclidean':
                                 # ((x1 - x2)^m + (y1 - y2)^m )^(1/m)
-                            self.similarity_matrix[i, j] = np.linalg.norm(a-b, ord=2)
+                            self.similarity_matrix[i, j] = 1/np.linalg.norm(a-b, ord=2)
                             self.similarity_matrix[j, i] = self.similarity_matrix[i, j]
                         elif similarity_metric == 'cityblock':
                                 # ((x1 - x2)^m + (y1 - y2)^m )^(1/m)
-                            self.similarity_matrix[i, j] = np.linalg.norm(a-b, ord=1)
+                            self.similarity_matrix[i, j] = 1/np.linalg.norm(a-b, ord=1)
                             self.similarity_matrix[j, i] = self.similarity_matrix[i, j]
                         elif similarity_metric == 'correlation':
                                 # ((x1 - x2)^m + (y1 - y2)^m )^(1/m)
@@ -175,11 +189,20 @@ class DistributionalModel:
                             raise AttributeError('Invalid arg "similarity_metric".')
             if i % 10 == 0:
                 print("    Finished {} rows".format(i))
-        np.savetxt(self.model_path+'/similarity_matrix.csv', self.similarity_matrix, delimiter=",", fmt="%0.4f")
+        np.savetxt(sim_file_path, self.similarity_matrix, delimiter=",", fmt="%0.4f")
     
-    def get_nearest_neighbors(self, n):
+    #############################################################################################################################
+    def get_nearest_neighbors(self, n, sim_file_path=None):
         print("Getting {} nearest neighbors".format(n))
-        f = open(self.model_path+'/nearest_neighbors.csv', 'w')
+
+        if self.similarity_matrix is None and sim_file_path is None:
+            raise AttributeError('No similarity matrix for this model exists. Load or create one.')
+        if self.similarity_matrix is None:
+            raise AttributeError('No similarity matrix exists. Load sim matrix functionality not implimented".')
+        if self.similarity_metric is None:
+            raise AttributeError('Similarity metric cannot be None')
+        
+        f = open(self.model_path + '/' + self.similarity_metric + '_nearest_neighbors.csv', 'w')
         n += 1 # this is to make sure we exclude the word's most similar word, itself
         if self.similarity_matrix is None:
             raise AttributeError('Must compute similarity matrix before computing nearest neighbors".')
